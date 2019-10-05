ed4ce00bec15f508b6bca031c3c4af7da5c7f113
JeroenBos <jer123bos@gmail.com>
2019-10-05 20:13:24 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 // TS:
 
-// export { OptionalToMissing, IsExact } from './typeHelper';
+export { IProvider } from './IoC/IProvider';
+export { IResettableProvider } from './IoC/IResettableProvider';
+export { AbstractProvider } from './IoC/AbstractProvider';
+export { InverseLookup } from './@types/TLookup';
+export { ResettableContainer } from './IoC/ResettableContainer';
+
 
 // JS:
 
@@ -11,6 +16,7 @@ import {
 import {
     undefinedToFalse
 } from './conversions';
+
 
 export default {
     assert, assertAreIdentical, undefinedToFalse
